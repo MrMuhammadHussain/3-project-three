fdaef1f5526d1107401935a77a1d7db01e6edcf6
mrmuhammadhussain <mugq678@gmail.com>
2023-01-29 00:00:30 +0500

    Components add Route Set

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Coins from './components/Coins'
+import Header from './components/Header'
+import Home from './components/Home'
+import Exchanges from './components/Exchanges'
+import CoinDetails from './components/CoinDetails'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home/> } />
+          <Route path='/coins' element={<Coins /> } />
+          <Route path='/exchanges' element={<Exchanges /> } />
+          <Route path='/coin/:id ' element={<CoinDetails /> } />
+
+
+
+
+
+
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+import {} from "react-router-dom"
+
+const Header = () => {
+  return (
+    <div>Header</div>
+  )
+}
+
+export default Header
