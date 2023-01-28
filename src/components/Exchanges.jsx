f1fd2c35e98fd408e52ea1492178bbe85af8a521
mrmuhammadhussain <mugq678@gmail.com>
2023-01-29 02:05:29 +0500

    some work

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import { Container, HStack } from '@chakra-ui/react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+import { server } from "../index"
+import ExchangeCard from './ExchangeCard'
+import Loader from "./Loader"
+
 
 const Exchanges = () => {
+
+  const [exchanges, setExchanges] = useState([])
+  const [Loading, setLoading] = useState(true)
+
+  useEffect(() => {
+
+    const fetchExchange = async () => {
+      const { data } = await axios.get(`${server}/exchanges`)
+      console.log(data);
+
+      setExchanges(data)
+      setLoading(false)
+    }
+    fetchExchange()
+
+  }, [])
+
+
+
   return (
-    <div>Exchanges</div>
+    <div>
+      <Container maxW={"container.xl"}>
+        {
+          Loading ? <Loader /> : <>
+            <HStack wrap={"wrap"}>
+
+              {
+                exchanges.map((i) =>
+                <ExchangeCard  name={i.name} image={i.image} url={i.url} rank={i.trust_score_rank} key={i.id} />
+                )
+              }
+            </HStack>
+
+          </>
+
+        }
+      </Container>
+
+
+
+    </div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,30 @@
 
-import {} from "react-router-dom"
+import { Button, HStack } from "@chakra-ui/react"
+import { Link } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+        <HStack p={"4"} shadow={"base"} backgroundColor={"blackAlpha.800"} spacing={"7"} justifyContent={"center"}>
+            <Button variant={"unstyled"} color={"white"}>
+                <Link to={"/"}>Home</Link>
+            </Button>
+
+
+            <Button variant={"unstyled"} color={"white"}>
+                <Link to={"/exchanges"}>Exchanges</Link>
+            </Button>
+
+
+            <Button variant={"unstyled"} color={"white"}>
+                <Link to={"/coins"}>Coins</Link>
+            </Button>
+    
+        
+        </HStack>
+
+
+    </div>
   )
 }
 
