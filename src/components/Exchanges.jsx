3571abe9af6561928250af0c200083340ca3cbc0
mrmuhammadhussain <mugq678@gmail.com>
2023-01-31 22:43:44 +0500

    Exchanges almost done

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -2,6 +2,7 @@ import { Container, HStack } from '@chakra-ui/react'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { server } from "../index"
+import ErrorComponent from './ErrorComponent'
 import ExchangeCard from './ExchangeCard'
 import Loader from "./Loader"
 
@@ -10,32 +11,42 @@ const Exchanges = () => {
 
   const [exchanges, setExchanges] = useState([])
   const [Loading, setLoading] = useState(true)
+  const [error , setError] = useState(false)
 
   useEffect(() => {
 
     const fetchExchange = async () => {
-      const { data } = await axios.get(`${server}/exchanges`)
-      console.log(data);
+      try {
+        const { data } = await axios.get(`${server}/exchanges`)
+        setExchanges(data)
+        setLoading(false)
 
-      setExchanges(data)
-      setLoading(false)
+      } catch (error) {
+        setError(true)
+        setLoading(false)
+        
+
+      }
     }
     fetchExchange()
 
   }, [])
+  if(error){
+    return <ErrorComponent />
+  }
 
 
 
   return (
-    <div>
-      <Container maxW={"container.xl"}>
+    <div >
+      <Container maxW={"container.xl"} pos={"absolute"} top={"20"}>
         {
           Loading ? <Loader /> : <>
             <HStack wrap={"wrap"}>
 
               {
                 exchanges.map((i) =>
-                <ExchangeCard  name={i.name} image={i.image} url={i.url} rank={i.trust_score_rank} key={i.id} />
+                  <ExchangeCard name={i.name} image={i.image} url={i.url} rank={i.trust_score_rank} key={i.id} />
                 )
               }
             </HStack>
