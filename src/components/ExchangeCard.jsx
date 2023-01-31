32e87b8d03f27741d0599668da998e707fd00b7a
mrmuhammadhussain <mugq678@gmail.com>
2023-01-31 22:44:35 +0500

    Exchange-Card

--- src/components/ExchangeCard.jsx
+++ src/components/ExchangeCard.jsx
@@ -1,9 +1,22 @@
+import { Heading, Image, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
-const ExchangeCard = ({name, image , rank , url}) => {
+const ExchangeCard = ({ name, image, rank, url }) => {
   return (
-    <div>
-    <div>{name}</div>
+    <div >
+      <a href={url} target="blank">
+        <VStack w={"52"} shadow={"lg"}  p={"8"} borderRadius={"lg"} transition={"all 0.3s"} m={"4"} css={{
+          "&:hover":{transform:"scale(1.1)"}
+        }}>
+          <Image src={image} w={"10"} h={"10"} objectFit={"contain"} alt={url} />
+          <Heading size={"md"} noOfLines={1} >{rank}</Heading>
+          <Text noOfLines={1}>{name}</Text>
+
+        </VStack>
+
+
+      </a>
+
     </div>
   )
 }
