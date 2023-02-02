68f3963d7da70f3da3c4a755b07ce88fef3644f2
mrmuhammadhussain <mugq678@gmail.com>
2023-02-03 01:28:26 +0500

    Make Loader with Chakra-Ui

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,24 @@
-import { Image } from '@chakra-ui/react'
+import { Box,  Spinner, VStack } from '@chakra-ui/react'
 import React from 'react'
-import gif from "../assets/coin.gif"
+// import gif from "../assets/coin.gif"
 
 const Loader = () => {
   return (
-    <div style={{display:"flex", justifyContent:"center",marginTop:"19%"}}> <Image src={gif} w={"300px"} h={"300px"} objectFit={"contain"} />
-    
-    
-    
-    
-    
+    <div>
+      {/* 1st loader */}
+      {/* <div style={{ display: "flex", justifyContent: "center", marginTop: "19%" }}> <Image src={gif} w={"300px"} h={"300px"} objectFit={"contain"} />
+      </div> */}
+      {/* ChakraUI Loader */}
+      <VStack h={"90vh"} justifyContent={"center"} >
+        <Box transform={"scale(3.9)"}>
+          <Spinner size={"xl"} />
+        </Box>
+      
+      </VStack>
+
+
+
+
     </div>
   )
 }
