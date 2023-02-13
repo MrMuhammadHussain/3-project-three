90ce70f7caddc84e56e4f39f48d79509cdbf38a5
mrmuhammadhussain <mugq678@gmail.com>
2023-02-13 20:01:41 +0500

    Make Home and Footer Compunents

---
+++ src/components/Footer.jsx
@@ -0,0 +1,32 @@
+import { Avatar, Box, Stack, Text, VStack } from '@chakra-ui/react'
+import founderSrc from '../assets/Founder.jpg'
+
+
+const Footer = () => {
+    return (
+        <div>
+            <Box bgColor={"blackAlpha.800"} color={"whiteAlpha.700"} minH={"48"} px={"16"} py={["16", "8"]}>
+                <Stack direction={["column", "row"]}
+                    h={"full"}
+                    alignItems={"center"}>
+                    <VStack w={"full"}
+                        alignItems={["center", "flex-start"]}>
+                        <Text fontWeight={"bold"}>About Us</Text>
+                        <Text fontSize={"sm"}
+                            letterSpacing={"widest"}
+                            textAlign={["center", "left"]}>I made This React CryptoApp as a Learing Project by Helping YouTube.</Text>
+                    </VStack>
+                    <VStack>
+                        <Avatar boxSize={"28"}
+                            mt={["4", "0"]}
+                            src={founderSrc} />
+                        <Text>Founder</Text>
+                    </VStack>
+                </Stack>
+            </Box>
+
+        </div>
+    )
+}
+
+export default Footer
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import { Box, Image, Text } from '@chakra-ui/react'
+import imgSrc from '../assets/btc.png'
+import { motion } from "framer-motion"
 
 const Home = () => {
   return (
-    <div>Home
+    <div>
+      <Box bgColor={"blackAlpha.900"} w={"full"} h={"85vh"}>
 
-{/* 6h:24m */}
+        <motion.div style={{
+          height: "80vh"
+        }}
+          animate={{
+            translateY: "12px"
+          }}
+          transition={{
+            duration: 1.5,
+            repeat: Infinity,
+            repeatType: "reverse"
+          }}
+        >
+          <Image w={"full"}
+            h={"full"}
+            objectFit={"contain"}
+            src={imgSrc}
+            filter={"grayscale(1)"} />
+        </motion.div>
+        <Text color={"whiteAlpha.800"}
+          fontSize={"6xl"}
+          textAlign={"center"}
+          fontWeight={"thin"}
+          mt={"-14"}>CryptoApp</Text>
+      </Box>
+
 
     </div>
   )
